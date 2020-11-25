fb0f6b2a8b6c5693e053deed38932e9b59e9a832
kishan <kishanykyada@gmail.com>
2020-11-25 15:12:41 +0530

    token user authenticate

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -12,7 +12,16 @@ class UserSerializers(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """create a new user with encrypted password and return it"""
-        return get_user_model().objects.create_user(**validated_data)   
+        return get_user_model().objects.create_user(**validated_data) 
+    
+    def update(self, instance, validated_data):
+        """update a user, setting the password correctly and return it."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user  
     
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for user authentication object"""
